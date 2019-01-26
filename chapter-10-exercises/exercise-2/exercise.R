24ba9f92bb8e0ed8822f1333cb421ea2c20a54c5
ttreat6 <46542557+ttreat6@users.noreply.github.com>
2019-01-25 18:10:26 -0800

    updated exercise-2
    had trouble doing this on terminal, had to do this manually...

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -4,28 +4,38 @@
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
 
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
 
+salary_2017 <- runif(100, 40000, 50000)
+salary_2017
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
 
+salary_adjustments <- runif(100, 5000, 10000)
+salary_adjustments
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
 
+salaries <- data.frame(employees, salary_2017, salary_adjustments, stringsAsFactors = FALSE)
+View(salaries)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
 
+salaries$salary_2018 <- salareis$salary_2017 + salaries$salary_adjustments
+View(salaries)
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
 
-
+salaries$got_raise <- salaries$salary_adjustments > 0
+View(salaries)
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
@@ -33,30 +43,46 @@
 
 # What was the 2018 salary of Employee 57
 
+salaries[57,"salary_2018"]
 
 # How many employees got a raise?
 
+nrow(salaries[salaries$got_raise == TRUE , ])
 
 # What was the dollar value of the highest raise?
 
+max(salaries$salary_adjustments)
 
 # What was the "name" of the employee who received the highest raise?
 
+salaries[salaries$salary_adjustments == max(salaries$salary_adjustments)  , "employees" ]
+
+# salaries$salary_adjustments == max(salaries$salary_adjustments) <- used to check
 
 # What was the largest decrease in salaries between the two years?
 
 
+
 # What was the name of the employee who recieved largest decrease in salary?
 
+    #There was no decrease in salary on the list. Used nrow(salaries[salaries$got_raise == TRUE , ])
+    #to check.
 
 # What was the average salary change?
 
+mean(salaries[["salary_adjustments"]])
 
 # For people who did not get a raise, how much money did they lose on average?
+    
+    #In my randomized dataset, everyone just happend to get a raise...
 
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
+    #I believe my averages matched. I did not have data for anyone who did not get a raise
+    # because all of my "employees" got raises.
+
 # Write a .csv file of your salary data to your working directory
 
+write.csv(salaries,'salary.csv')
